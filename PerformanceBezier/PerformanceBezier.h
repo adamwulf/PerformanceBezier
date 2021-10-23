b3cc82221836a8ce1f0692033d58b1ff836c57f8
Adam Wulf <adam.wulf@gmail.com>
2021-10-22 19:04:00 -0500

    remove cocoapods support

--- PerformanceBezier/PerformanceBezier.h
+++ PerformanceBezier/PerformanceBezier.h
@@ -8,12 +8,6 @@
 
 #define CGPointNotFound CGPointMake(CGFLOAT_MAX, CGFLOAT_MAX)
 
-// SwiftPackageManager needs imports to be "", while frameworks generally want <>
-// It's unfortunately not enough to `#if SWIFT_PACKAGE`, as if PerformanceBezier
-// is a dependency of another SPM package, then the SWIFT_PACKAGE seems to not be
-// set when building that dependency. So instead, we'll choose based off of Cocoapods
-
-#if SWIFT_PACKAGE || !COCOAPODS
 #import "UIBezierPath+Center.h"
 #import "UIBezierPath+Clockwise.h"
 #import "UIBezierPath+Description.h"
@@ -25,16 +19,3 @@
 #import "UIBezierPath+Util.h"
 #import "UIBezierPathProperties.h"
 #import <Foundation/Foundation.h>
-#else
-#import <PerformanceBezier/UIBezierPath+Center.h>
-#import <PerformanceBezier/UIBezierPath+Clockwise.h>
-#import <PerformanceBezier/UIBezierPath+Description.h>
-#import <PerformanceBezier/UIBezierPath+Equals.h>
-#import <PerformanceBezier/UIBezierPath+NSOSX.h>
-#import <PerformanceBezier/UIBezierPath+Ahmed.h>
-#import <PerformanceBezier/UIBezierPath+Performance.h>
-#import <PerformanceBezier/UIBezierPath+Trim.h>
-#import <PerformanceBezier/UIBezierPath+Util.h>
-#import <PerformanceBezier/UIBezierPathProperties.h>
-#import <Foundation/Foundation.h>
-#endif
